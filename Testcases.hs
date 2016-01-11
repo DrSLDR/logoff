420c6cd740647df5cd3178b60cc463d53792141d
Jonas A. Hulten <jonas@sldr.se>
2016-01-11 21:46:00 +0100

    Overhauled testing framework

--- Testcases.hs
+++ Testcases.hs
@@ -5,45 +5,88 @@
 
 import Logoff
 import Datatypes
+import Control.Monad
+import System.Exit
 
+{------------------------------------------------------------------------------}
+-- Framework operative block
 {------------------------------------------------------------------------------}
 -- Dummy main function
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+  result <- ioize res
+  unless (evaluateResult res) exitFailure
+  where res = map evaluateTest axTests
+
+-- Testing datatype
+type Test = (Bool,Bool,String)
+
+-- Result datatype
+type Result = (Bool,String)
+
+-- Test evaluator function (for verbose output)
+evaluateTest :: Test -> Result
+evaluateTest (t,i,l) = if t == i
+  then (True, l ++ "... Pass")
+  else (False,l ++ "... FAIL")
+
+-- String compactor and IOizer
+ioize :: [Result] -> IO [()]
+ioize = mapM (putStrLn . snd)
+
+-- Full run evaluator (for correct output)
+evaluateResult :: [Result] -> Bool
+evaluateResult = all fst
+
+-- Master test list
+tests :: [Test]
+tests = axTests
 
 {------------------------------------------------------------------------------}
+-- Axiom block
+{------------------------------------------------------------------------------}
 -- Sample Ax 1 - supposed to succeed
-ax_1 :: Sequent
-ax_1 = Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructureF (Positive (Atomic 'x')))
+ax_1 :: Test
+ax_1 = (ax_1_test, ax_1_intendedOutcome, ax_1_label)
+
+ax_1_label :: String
+ax_1_label = "Trivial Ax-1"
 
 ax_1_intendedOutcome :: Bool
 ax_1_intendedOutcome = True
 
-test_ax_1 :: Bool
-test_ax_1 = isAx ax_1
+ax_1_test :: Bool
+ax_1_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
+  (OStructureF (Positive (Atomic 'x'))))
 {------------------------------------------------------------------------------}
-
 -- Sample Ax 2 - supposed to fail
-ax_2 :: Sequent
-ax_2 = Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructureF (Positive (Atomic 'y')))
+ax_2 :: Test
+ax_2 = (ax_2_test, ax_2_intendedOutcome, ax_2_label)
+
+ax_2_label :: String
+ax_2_label = "Trivial Ax-2"
 
 ax_2_intendedOutcome :: Bool
 ax_2_intendedOutcome = False
 
-test_ax_2 :: Bool
-test_ax_2 = isAx ax_2
+ax_2_test :: Bool
+ax_2_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
+  (OStructureF (Positive (Atomic 'y'))))
 {------------------------------------------------------------------------------}
-
 -- Sample Ax 3 - supposed to fail
-ax_3 :: Sequent
-ax_3 = Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructure (Positive (Atomic 'x')))
+ax_3 :: Test
+ax_3 = (ax_3_test, ax_3_intendedOutcome, ax_3_label)
+
+ax_3_label :: String
+ax_3_label = "Trivial Ax-3"
 
 ax_3_intendedOutcome :: Bool
 ax_3_intendedOutcome = False
 
-test_ax_3 :: Bool
-test_ax_3 = isAx ax_3
+ax_3_test :: Bool
+ax_3_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
+  (OStructure (Positive (Atomic 'x'))))
 {------------------------------------------------------------------------------}
+-- Ax test list
+axTests :: [Test]
+axTests = [ax_1,ax_2,ax_3]
