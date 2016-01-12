dd322a197303647a08ac7a0850eaf12139a0e78b
Jonas A. Hultén <jonas@sldr.se>
2016-01-12 11:56:32 +0100

    Fixed and prettyfied testcases

--- Testcases.hs
+++ Testcases.hs
@@ -45,117 +45,111 @@ tests = axTests
 {------------------------------------------------------------------------------}
 -- Axiom block
 {------------------------------------------------------------------------------}
--- Sample Ax 1 - Trivial, correct, atomic Ax-axiom
+-- Trivial Ax-1 - Trivial, correct, atomic Ax-axiom
+-- x+ |- [x+]
 ax_1 :: Test
-ax_1 = (ax_1_test, ax_1_intendedOutcome, ax_1_label)
-
-ax_1_label :: String
-ax_1_label = "Trivial Ax-1"
-
-ax_1_intendedOutcome :: Bool
-ax_1_intendedOutcome = True
-
-ax_1_test :: Bool
-ax_1_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructureF (Positive (Atomic 'x'))))
+ax_1 = (isAx
+  (Sequent
+    (IStructure
+      (Positive (Atomic 'x')))
+    (OStructureF
+      (Positive (Atomic 'x')))),
+  True,
+  "Trivial Ax-1"
+  )
 {------------------------------------------------------------------------------}
--- Sample Ax 2 - Simple, non-matching atomic Ax-axiom
+-- Trivial Ax-2 - Simple, non-matching atomic Ax-axiom
+-- x+ |- [y+]
 ax_2 :: Test
-ax_2 = (ax_2_test, ax_2_intendedOutcome, ax_2_label)
-
-ax_2_label :: String
-ax_2_label = "Trivial Ax-2"
-
-ax_2_intendedOutcome :: Bool
-ax_2_intendedOutcome = False
-
-ax_2_test :: Bool
-ax_2_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructureF (Positive (Atomic 'y'))))
+ax_2 = (isAx
+  (Sequent
+    (IStructure
+      (Positive (Atomic 'x')))
+    (OStructureF
+      (Positive (Atomic 'y')))),
+  False,
+  "Trivial Ax-2"
+  )
 {------------------------------------------------------------------------------}
--- Sample Ax 3 - Simple, non-focused, matching Ax-axiom
+-- Trivial Ax-3 - Simple, non-focused, matching Ax-axiom
+-- x+ |- x+
 ax_3 :: Test
-ax_3 = (ax_3_test, ax_3_intendedOutcome, ax_3_label)
-
-ax_3_label :: String
-ax_3_label = "Trivial Ax-3"
-
-ax_3_intendedOutcome :: Bool
-ax_3_intendedOutcome = False
-
-ax_3_test :: Bool
-ax_3_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructure (Positive (Atomic 'x'))))
+ax_3 = (isAx
+  (Sequent
+    (IStructure
+      (Positive (Atomic 'x')))
+    (OStructure
+      (Positive (Atomic 'x')))),
+  False,
+  "Trivial Ax-3"
+  )
 {------------------------------------------------------------------------------}
--- Sample Ax 4 - Simple, atom-to-non-atom Ax-axiom
+-- Trivial Ax-4 - Simple, atom-to-non-atom Ax-axiom
+-- x+ |- [(x+ (tensor) x+)+]
 ax_4 :: Test
-ax_4 = (ax_4_test, ax_4_intendedOutcome, ax_4_label)
-
-ax_4_label :: String
-ax_4_label = "Trivial Ax-4"
-
-ax_4_intendedOutcome :: Bool
-ax_4_intendedOutcome = False
-
-ax_4_test :: Bool
-ax_4_test = isAx (Sequent (IStructure (Positive (Atomic 'x')))
-  (OStructureF (Positive (Tensor (Atomic 'x') (Atomic 'x')))))
+ax_4 = (isAx
+  (Sequent
+    (IStructure
+      (Positive (Atomic 'x')))
+    (OStructureF
+      (Positive (Tensor
+        (Positive (Atomic 'x'))
+        (Positive (Atomic 'x')))))),
+  False,
+  "Trivial Ax-4"
+  )
 {------------------------------------------------------------------------------}
--- Sample CoAx 1 - Trivial, correct, atomic CoAx-axiom
+-- Trivial CoAx-1 - Trivial, correct, atomic CoAx-axiom
+-- [x-] |- x-
 coAx_1 :: Test
-coAx_1 = (coAx_1_test, coAx_1_intendedOutcome, coAx_1_label)
-
-coAx_1_label :: String
-coAx_1_label = "Trivial CoAx-1"
-
-coAx_1_intendedOutcome :: Bool
-coAx_1_intendedOutcome = True
-
-coAx_1_test :: Bool
-coAx_1_test = isCoAx (Sequent (IStructureF (Negative (Atomic 'x')))
-  (OStructure (Negative (Atomic 'x'))))
+coAx_1 = (isCoAx
+  (Sequent
+    (IStructureF
+      (Negative (Atomic 'x')))
+    (OStructure
+      (Negative (Atomic 'x')))),
+  True,
+  "Trivial CoAx-1"
+  )
 {------------------------------------------------------------------------------}
--- Sample CoAx 2 - Simple, non-matching atomic Ax-axiom
+-- Trivial CoAx-2 - Simple, non-matching atomic Ax-axiom
+-- [x-] |- y-
 coAx_2 :: Test
-coAx_2 = (coAx_2_test, coAx_2_intendedOutcome, coAx_2_label)
-
-coAx_2_label :: String
-coAx_2_label = "Trivial CoAx-2"
-
-coAx_2_intendedOutcome :: Bool
-coAx_2_intendedOutcome = False
-
-coAx_2_test :: Bool
-coAx_2_test = isCoAx (Sequent (IStructureF (Negative (Atomic 'x')))
-  (OStructure (Negative (Atomic 'y'))))
+coAx_2 = (isCoAx
+  (Sequent
+    (IStructureF
+      (Negative (Atomic 'x')))
+    (OStructure
+      (Negative (Atomic 'y'))))
+  False,
+  "Trivial CoAx-2"
+  )
 {------------------------------------------------------------------------------}
--- Sample CoAx 3 - Simple, non-focused, matching Ax-axiom
+-- Trivial CoAx-3 - Simple, non-focused, matching Ax-axiom
 coAx_3 :: Test
-coAx_3 = (coAx_3_test, coAx_3_intendedOutcome, coAx_3_label)
-
-coAx_3_label :: String
-coAx_3_label = "Trivial CoAx-3"
-
-coAx_3_intendedOutcome :: Bool
-coAx_3_intendedOutcome = False
-
-coAx_3_test :: Bool
-coAx_3_test = isCoAx (Sequent (IStructure (Negative (Atomic 'x')))
-  (OStructure (Negative (Atomic 'x'))))
+coAx_3 = (isCoAx
+  (Sequent
+    (IStructure
+      (Negative (Atomic 'x')))
+    (OStructure
+      (Negative (Atomic 'x'))))
+  False,
+  "Trivial CoAx-3"
+  )
 {------------------------------------------------------------------------------}
--- Sample CoAx 4 - Simple, atom-to-non-atom Ax-axiom
+-- Trivial CoAx-4 - Simple, atom-to-non-atom Ax-axiom
 coAx_4 :: Test
-coAx_4 = (coAx_4_test, coAx_4_intendedOutcome, coAx_4_label)
-
-coAx_4_label :: String
-coAx_4_label = "Trivial CoAx-4"
-
-coAx_4_intendedOutcome :: Bool
-coAx_4_intendedOutcome = False
-
-coAx_4_test :: Bool
-coAx_4_test = isCoAx (Sequent (IStructureF (Negative (Atomic 'x')))
-  (OStructure (Positive (Tensor (Atomic 'x') (Atomic 'x')))))
+coAx_4 = (isCoAx
+  (Sequent
+    (IStructureF
+      (Negative (Atomic 'x')))
+    (OStructure
+      (Positive (Tensor
+        (Negative (Atomic 'x'))
+        (Negative (Atomic 'x'))))))
+  False,
+  "Trivial CoAx-4"
+  )
 {------------------------------------------------------------------------------}
 -- Ax test list
 axTests :: [Test]
